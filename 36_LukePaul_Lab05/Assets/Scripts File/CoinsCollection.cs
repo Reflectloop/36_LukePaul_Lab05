877331b6cc8cafc69830a2cfa8c0478268fe5e12
Reflectloop <135951011+Reflectloop@users.noreply.github.com>
2024-01-10 11:14:54 +0800

    Added Unity Project
    Level 1 added

---
+++ 36_LukePaul_Lab05/Assets/Scripts File/CoinsCollection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CoinsCollection : MonoBehaviour
+{
+
+    private int Coin = 0;
+    public TextMeshProUGUI coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coins"))
+        {
+
+            Coin++;
+            coinText.text = "Coin: " + Coin.ToString();
+            Debug.Log(Coin);
+            Destroy(other.gameObject);
+            if (Coin == 4)
+            {
+                SceneManager.LoadScene("WinScene");
+            }
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 36_LukePaul_Lab05/Assets/Scripts File/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody playerRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(MoveHorizontal, 0, MoveVertical);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Killbrick")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
